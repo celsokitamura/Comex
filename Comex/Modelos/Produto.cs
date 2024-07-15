93d447f675cce67128a549f29a31d54f520f9fb2
Celso Kitamura <celso@kitamuraconsultoria.com.br>
2024-07-15 20:33:16 -0300

    Implentações da Semana 2

--- Comex/Modelos/Produto.cs
+++ Comex/Modelos/Produto.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Comex.Modelos
 {
     internal class Produto
     {
-        public Produto(string nome, decimal preco)
+        public Produto(int id, string nome, decimal preco, string descricao, string categoria)
         {
+            Id = id;
             Nome = nome;
             Preco = preco;
+            Descricao = descricao;
+            Categoria = categoria;
         }
 
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        
+        [JsonPropertyName("title")] 
         public string Nome { get; set; }
+        
+        [JsonPropertyName("price")]
         public decimal Preco { get; set; }
+        
+        [JsonPropertyName("description")] 
+        public string Descricao { get; set; }
+        
+        [JsonPropertyName("category")]
+        public string Categoria { get; set; }
+
     }
 }
--- Comex/Program.cs
+++ Comex/Program.cs
@@ -1,14 +1,16 @@
 ﻿using Comex.Funcionalidades;
 using Comex.Modelos;
 using System.Collections.Generic;
+using System.Text.Json;
 
 Dictionary<int, Funcionalidade> funcionalidades = new();
 
 funcionalidades.Add(1, new CadastraCliente("Cadastrar Cliente"));
 funcionalidades.Add(2, new ListaCliente("Listar Cliente"));
 funcionalidades.Add(3, new CadastraProduto("Cadastrar Produto"));
-funcionalidades.Add(4, new AjustaPrecoDeProduto("Ajustar Preço do Produto"));
-funcionalidades.Add(5, new AdicionaProdutoNoCarrinho("Adicionar produto no carrinho"));
+funcionalidades.Add(4, new ConsultaProduto("Listar Produto por Categoria"));
+funcionalidades.Add(5, new AjustaPrecoDeProduto("Ajustar Preço do Produto"));
+funcionalidades.Add(6, new AdicionaProdutoNoCarrinho("Adicionar produto no carrinho"));
 funcionalidades.Add(-1, new Sair("Sair"));
 
 
@@ -16,15 +18,35 @@ Dictionary<string, Cliente> clientes = new();
 Dictionary<string, Produto> produtos = new();
 Dictionary<string, Carrinho> carrinhos = new();
 
+using(HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://fakestoreapi.com/products");
+        var listaProdutos = JsonSerializer.Deserialize<List<Produto>>(resposta);
+
+        foreach (var item in listaProdutos)
+        {
+            produtos.Add(item.Nome.Trim(), new Produto(item.Id, item.Nome.Trim(), item.Preco, item.Descricao, item.Categoria));
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Erro no carregamento dos produtos: {ex.Message}");
+    }
+}
+
+
 
 void ExibirMenuDeOpcoes()
 {
     Console.WriteLine("BEM VINDO A COMEX!\n");
     Console.WriteLine("Digite 1 para Cadastrar um cliente");
     Console.WriteLine("Digite 2 para Listar clientes");
-    Console.WriteLine("Digite 3 para Cadastrar um produto");
-    Console.WriteLine("Digite 4 para Ajustar o Preço de um produto");
-    Console.WriteLine("Digite 5 para Adicionar produto no carrinho");
+    //Console.WriteLine("Digite 3 para Cadastrar um produto");
+    Console.WriteLine("Digite 4 para Listar Produto por Categoria");
+    Console.WriteLine("Digite 5 para Ajustar o Preço de um produto");
+    Console.WriteLine("Digite 6 para Adicionar produto no carrinho");
     Console.WriteLine("Digite -1 pra sair");
 
     Console.WriteLine("\nDigite a sua opção: ");
@@ -43,9 +65,10 @@ void ExibirMenuDeOpcoes()
                 break;
             case 3:
             case 4:
+            case 5:
                 menuASerExibido.Executar(produtos);
                 break;
-            case 5:
+            case 6:
                 menuASerExibido.Executar(clientes, produtos, carrinhos);
                 break;
             default:
