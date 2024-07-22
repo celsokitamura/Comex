6c0164499287ff173e9ada59d38b481546a02970
Celso Kitamura <celso@kitamuraconsultoria.com.br>
2024-07-22 20:10:19 -0300

    Implementações da semana 4

---
+++ Comex/Funcionalidades/CarregarListaDeProdutos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Comex.Modelos;
+
+namespace Comex.Funcionalidades
+{
+    internal class CarregarListaDeProdutos : Funcionalidade
+    {
+        public CarregarListaDeProdutos(string titulo) : base(titulo)
+        { }
+
+        public override void Executar(Dictionary<string, Produto> produtos)
+        {
+            base.Executar(produtos);
+            ExibirTitulo();
+
+            if (!File.Exists("CargaProdutos.txt"))
+            {
+                Console.WriteLine("Arquivo de produtos não encontrado.");
+                return;
+            }
+
+            produtos.Clear();
+
+            using (StreamReader reader = new StreamReader("CargaProdutos.txt"))
+            {
+                string linha;
+                while ((linha = reader.ReadLine()) != null)
+                {
+                    string[] partes = linha.Split("||"); //id;title;price;description;category
+
+                    if (partes.Length == 5)
+                    {
+                        int id;
+                        string nome = partes[1];
+                        string descricao = partes[2];
+                        decimal preco;
+                        string categoria = partes[4];
+
+                        if (int.TryParse(partes[0], out id) && decimal.TryParse(partes[3], out preco))
+                        {
+                            produtos.Add(nome, new Produto(id, nome, preco, descricao, categoria));
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Lista de produtos carregada com sucesso.");
+
+            Console.WriteLine("Digite um tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
---
+++ Comex/Funcionalidades/SalvarListaDeProdutos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Comex.Modelos;
+
+namespace Comex.Funcionalidades
+{
+    internal class SalvarListaDeProdutos : Funcionalidade
+    {
+        public SalvarListaDeProdutos(string titulo) : base(titulo)
+        { }
+
+        public override void Executar(Dictionary<string, Produto> produtos)
+        {
+            base.Executar(produtos);
+            ExibirTitulo();
+
+            using (StreamWriter writer = new StreamWriter("produtos.txt"))
+            {
+                foreach (var produto in produtos.Values)
+                {
+                    writer.WriteLine($"{produto.Id}||{produto.Nome}||{produto.Descricao}||{produto.Preco}||{produto.Categoria}");
+                }
+            }
+            Console.WriteLine("Lista de produtos saqlva com sucesso.");
+
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
