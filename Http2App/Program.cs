35c44fd45639f9bebe20c9206215bd127af2c08a
Celso Kitamura <celso@kitamuraconsultoria.com.br>
2024-08-01 21:03:59 -0300

    Implementações da semana 9

---
+++ Http2App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Http2App
+{
+    class Program
+    {
+        static async Task Main(string[] args) 
+        { 
+            using (HttpClient client = new HttpClient()) 
+            {
+                //client.DefaultRequestVersion = new Version(2, 0);
+                client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher;
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                HttpResponseMessage response = await client.GetAsync("https://http2.akamai.com/demo");
+                stopwatch.Stop();
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Status Code: {response.StatusCode}");
+                Console.WriteLine($"Protocol Version: {response.Version}");
+                Console.WriteLine($"Response Time: {stopwatch.ElapsedMilliseconds} ms");
+                Console.WriteLine("Response Body: ");
+                Console.WriteLine(responseBody);
+            }
+        }
+    }
+}
